34eccd5e404d49433326c5e96e27c74b3109ff5d
葉狩康憲 <hagarikouken@hagarikoukennoMacBook-Pro.local>
2019-12-27 18:25:12 +0900

    各ページ作成

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,26 +1,46 @@
 class BooksController < ApplicationController
-  def new
-    @book = Book.new
-  end
-
   def create
     @book = Book.new(book_params)
-    @book.save
-    redirect_to books_path
+    if @book.save
+      flash[:notice] = "Posted Ok"
+      redirect_to book_path(@book)
+    else
+      redirect_to books_path
+      flash[:notice] = "Posted Failed"
+    end
   end
 
   def root
   end
 
   def index
-    @book = Book.all
-    
+    @books = Book.all
+    @book = Book.new
   end
 
   def show
+    @book = Book.find(params[:id])
   end
 
   def edit
+    @book = Book.find(params[:id])
+  end
+
+  def update
+    @book = Book.find(params[:id])
+    if @book.update(book_params)
+      flash[:notice] = "Puted Ok"
+       redirect_to book_path
+    else
+      redirect_to edit_book_path(@book)
+      flash[:notice] = "Puted Failed"
+    end
+  end
+
+  def destroy
+    @book = Book.find(params[:id])
+    @book.destroy
+    redirect_to books_path
   end
 
   private
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,7 @@
 Rails.application.routes.draw do
-  get 'books' => "books#index"
+  # get 'books' => "books#index"
   root 'books#root'
-  get 'books/new' => 'books#new'
-  post 'books' => 'books#create'
-  get 'books/:id' =>'books#show'
-  get 'books/:id/edit' =>'books#edit'
+  resources :books
+  
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
